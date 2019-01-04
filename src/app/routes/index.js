a5e453db3b5aa9f4ba4db06e02257bb0bbc299aa
Breno França <franciscobreno.si@gmail.com>
2019-01-04 14:32:31 -0300

    usuario_autenticacao - adicionado o controle de redirecionamento quando o usuario esta logado

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -4,19 +4,20 @@ const upload = require('multer')(multerConfig)
 
 const router = express.Router()
 
+const authMiddleware = require('../middlewares/auth')
+const guestMiddleware = require('../middlewares/guest')
+
 const UserController = require('../controllers/UserController')
 const SessionController = require('../controllers/SessionController')
 
-router.get('/', SessionController.create)
+router.get('/', guestMiddleware, SessionController.create)
 
-router.get('/signup', UserController.create)
+router.get('/signup', guestMiddleware, UserController.create)
 router.post('/signup', upload.single('avatar'), UserController.store)
+router.post('/signin', guestMiddleware, SessionController.store)
 
-router.post('/signin', SessionController.store)
+router.use('/app', authMiddleware)
 
-router.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
-  return res.render('dashboard')
-})
+router.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
 module.exports = router
