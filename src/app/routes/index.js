42f657f5d7da6c5ce78e630db7086708da82a233
Breno França <franciscobreno.si@gmail.com>
2019-01-04 10:38:22 -0300

    ajustes gerais - ajustes na estrutura de pastas e conffiguracao de layouts

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function(req, res) {
-  res.render("index", { title: "NodeJS" });
-});
+router.get("/", (req, res) => res.render("index", { title: "NodeJS" }));
+
+router.get("/signup", (req, res) => res.render("auth/signup"));
 
 module.exports = router;
