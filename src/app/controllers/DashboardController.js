0cf9c42caeb7041b2d9769aef48d713908f92d60
Breno França <franciscobreno.si@gmail.com>
2019-01-05 21:02:03 -0300

    providers_dashboard
     - adicionado area de acompanhamento de agendamentos dos prestadores de servico
     - ajuste no layout

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,10 +1,38 @@
-const { User } = require('../models')
+const moment = require('moment')
+const { Op } = require('sequelize')
+const { User, Appointment } = require('../models')
 
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
+    const date = moment()
+    const { user } = req.session
 
-    return res.render('dashboard', { providers })
+    if (!user.provider) {
+      const providers = await User.findAll({ where: { provider: true } })
+      return res.render('dashboard/clients', { providers })
+    }
+
+    let appointments = await Appointment.findAll({
+      where: {
+        provider_id: user.id,
+        date: {
+          [Op.between]: [
+            date.startOf('day').format(),
+            date.endOf('day').format()
+          ]
+        }
+      },
+      include: [
+        {
+          model: User,
+          as: 'user'
+        }
+      ]
+    })
+
+    console.log(appointments)
+
+    return res.render('dashboard/providers', { appointments })
   }
 }
 
--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,7 @@ module.exports = {
   database: 'bootcamp_challenge_2',
   host: '127.0.0.1',
   operatorsAlias: false,
+  timezone: 'America/Fortaleza',
   define: {
     timestamps: true,
     underscored: true,
